7502c497eb2a54ef1c5a7fedd8810333611335fe
A Student <student@nowhere.edu>
2026-02-11 02:41:05 +0000

    Feb 10, 2026, 6:41 PM

--- main.cpp
+++ main.cpp
@@ -15,7 +15,17 @@ double getShippingCost(int method, double orderCost){
     }
   }
 
+  else if (method == 2){
+    if (orderCost <= 300){
+      return 0.02 * orderCost + 10;
+    }
+    else{
+      return 0;
+    }
+  }
+
   
+
 }
 
 int main()
