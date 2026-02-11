027c7531c8777aa737f196d01b988eef2a0db53d
A Student <student@nowhere.edu>
2026-02-11 02:38:58 +0000

    Feb 10, 2026, 6:38 PM

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 double getShippingCost(int method, double orderCost){
+  if (method == 1){
+    if (orderCost < 100){
+      return -1;
+    }
+    else if (orderCost < 500){
+      return 50;
+    }
+    else{
+      return 0.1 * orderCost;
+    }
+  }
+
   
 }
 
